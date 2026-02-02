3965392812be765fd1ec1bb541c13deb5bb7a614
zahid096 <mdzahidhasan096@gmail.com>
2026-02-02 15:34:47 +0600

    Complete Task Analysis

---
+++ Advance_analysis.sql
@@ -0,0 +1,43 @@
+/*
+Task 13: 
+Identify Members with Overdue Books
+Write a query to identify members who have overdue books (assume a 30-day return period). 
+Display the member's_id, member's name, book title, issue date, and days overdue.
+*/
+
+-- issued_status == members == books == return_status
+-- filter books which is return
+-- overdue > 30
+
+/*    
+Task 14: Update Book Status on Return
+Write a query to update the status of books in the books table to "Yes" when they are returned (based on entries in the return_status table).
+*/
+
+/*
+Task 15: Branch Performance Report
+Create a query that generates a performance report for each branch, showing the number of books issued, the number of books returned, and the total revenue generated from book rentals.
+*/
+
+-- Task 16: CTAS: Create a Table of Active Members
+-- Use the CREATE TABLE AS (CTAS) statement to create a new table active_members containing members who have issued at least one book in the last 2 months.
+
+--
+-- Task 17: Find Employees with the Most Book Issues Processed
+-- Write a query to find the top 3 employees who have processed the most book issues. Display the employee name, number of books processed, and their branch.
+
+/* Task 19: Stored Procedure Objective:
+Create a stored procedure to manage the status of books in a library system. 
+
+Description: Write a stored procedure that updates the status of a book in the library based on its issuance. 
+
+The procedure should function as follows: 
+
+The stored procedure should take the book_id as an input parameter. 
+
+The procedure should first check if the book is available (status = 'yes'). 
+
+If the book is available, it should be issued, and the status in the books table should be updated to 'no'. 
+
+If the book is not available (status = 'no'), the procedure should return an error message indicating that the book is currently not available.
+*/
