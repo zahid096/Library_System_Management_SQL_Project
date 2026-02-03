e70c2d82f85e91e31c7dfb25654d85b9f8ed70ff
zahid096 <mdzahidhasan096@gmail.com>
2026-02-03 08:32:51 +0600

    Update Database

--- Advance_analysis.sql
+++ Advance_analysis.sql
@@ -1,6 +1,5 @@
 /*
-Task 13: 
-Identify Members with Overdue Books
+Task 13: Identify Members with Overdue Books
 Write a query to identify members who have overdue books (assume a 30-day return period). 
 Display the member's_id, member's name, book title, issue date, and days overdue.
 */
